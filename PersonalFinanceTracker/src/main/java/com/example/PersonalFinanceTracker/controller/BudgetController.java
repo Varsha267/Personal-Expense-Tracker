a825c602bcba4e411b1f08d99e1e8171d7137080
Varsha267 <118620368+Varsha267@users.noreply.github.com>
2026-03-10 23:02:17 +0530

    expense

--- PersonalFinanceTracker/src/main/java/com/example/PersonalFinanceTracker/controller/BudgetController.java
+++ PersonalFinanceTracker/src/main/java/com/example/PersonalFinanceTracker/controller/BudgetController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","https://personal-expense-tracker-ruby.vercel.app"})
 @RestController
 @RequestMapping("/api/budget")
 @RequiredArgsConstructor
